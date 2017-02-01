7521049dc1c8552352690c5213aa6d58f6dd113c
Wataru Manji <wataru@manjiya.net>
2017-02-01 22:40:06 +0900

    fix

--- hackmd/config.js
+++ hackmd/config.js
@@ -2,8 +2,6 @@
 var domain = ''; // domain name
 var urlpath = ''; // sub url path, like: www.example.com/<urlpath>
 //settings
-var debug = false;
-
 var port = '80';
 var usecdn = false;
 var GOOGLE_API_KEY = '';
--- hackmd/docker-entrypoint.sh
+++ hackmd/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/dash
 
+export NODE_ENV='production'
+
 if [ -f .sequelizerc ];
 then
     node_modules/.bin/sequelize db:migrate
